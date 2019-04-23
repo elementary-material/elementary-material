8de3a9e5d44f2bd2f012ac5df65c21fe548761c3
Matheus <matheusalexandre.sena99@gmail.com>
2019-04-23 09:12:22 -0300

    Responsive Navbar without JavaScript

--- grid.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById("toggle-menu");
-var navBurgerState = false;
-var navList = document.getElementById("toggle-list");
-
-button.addEventListener("click", () => {
-    navBurgerState = !navBurgerState;
-    (navBurgerState) ? navList.style.cssText = "display: block" : navList.style.cssText = "display: none";
-})
