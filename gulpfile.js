074fa2ffce5367a32f4b3379f0d8310b6d065e57
Matheus <matheusalexandre.sena99@gmail.com>
2019-06-21 12:25:24 -0300

    Setting gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
+const cleanCSS = require('gulp-clean-css');
 
+gulp.task('concat', () => {
+    return gulp.src('./src/styles/*.css').pipe(concat('all.css')).pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('minify', () => {
+    return gulp.src('./dist/*.css').pipe(cleanCSS({
+        compatibility: 'ie8',
+        level: {
+            2: {
+                specialComments: 0,
+                all: true
+            }
+        },
+        inline: ['all']
+    })).pipe(gulp.dest('./dist/'))
+})
