ecbf0f10b1d7b2f0103e0f03ddd282edeb8ab636
Matheus <matheusalexandre.sena99@gmail.com>
2019-06-22 10:28:14 -0300

    Navbar control state script

--- src/js/_navbar.js
+++ src/js/_navbar.js
@@ -1,1 +1,20 @@
-"use strict";
+"use strict"
+
+window.onload = (() => {
+
+    const overlay = document.getElementById("overlay")
+    const navigations = document.getElementsByClassName("nav-link")
+
+    overlay.addEventListener('click', () => {
+        closeNav()
+    })
+
+    Object.keys(navigations).map(i => {
+        navigations.item(i).onclick = closeNav
+    })
+
+    function closeNav() {
+        document.getElementById("btn-menu").checked = false
+    }
+
+})
