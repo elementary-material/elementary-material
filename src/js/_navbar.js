5fa1b3eb16b1a9c913aa180bf8fbeca46447acf6
Matheus <matheusalexandre.sena99@gmail.com>
2019-07-07 23:40:32 -0300

    Starts the support to galleries

--- src/js/_navbar.js
+++ src/js/_navbar.js
@@ -4,15 +4,18 @@ window.onload = (() => {
 
     const overlay = document.getElementById("overlay")
     const navigations = document.getElementsByClassName("nav-link")
-    
 
-    overlay.addEventListener('click', () => {
-        closeNav()
-    })
+    if (overlay) {
+        overlay.addEventListener('click', () => {
+            closeNav()
+        })
+    }
 
-    Object.keys(navigations).map(i => {
-        navigations.item(i).onclick = closeNav
-    })
+    if (navigations) {
+        Object.keys(navigations).map(i => {
+            navigations.item(i).onclick = closeNav
+        })
+    }
 
     function closeNav() {
         document.getElementById("btn-menu").checked = false
